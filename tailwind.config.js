7e9b7a84002974b58664f4e2b953564ffa3c2c16
Derin Owoade <owoadederin6@gmail.com>
2022-10-28 12:41:13 +0100

    Tailwind confing

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ const Myclass = plugin(function ({ addUtilities }) {
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
